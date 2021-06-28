16664ca025b9bd3ad045e4bc86c0972f09141f0c
sad301 <sad301@gmail.com>
2021-06-28 15:11:33 +0800

    qqq

--- ngeprint/customer_routes.py
+++ ngeprint/customer_routes.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import request, render_template
 from ngeprint import app
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+	if request.method == "POST":
+		return "this is post method"
 	return render_template("index.html.j2")
--- ngeprint/static/q.js
+++ ngeprint/static/q.js
@@ -1,13 +1,46 @@
 function _() {
-	this.index = function () {
-		$('input[type=file]').change(e => {
-			let f = e.target.files[0];
-			$('#txt-dokumen').val(f.name);
-		});
-		$('#btn-select-file').click(e => {
-			$('input[type="file"]').trigger('click');
-		});
-	}
+
+	this.index = {
+		init: () => {
+
+			$('input[type="text"]').focus(e => {
+				$(e.target).closest('.field').removeClass('error');
+				if(e.target.id == 'txt-dokumen') {
+					$('#btn-select-file').removeClass('negative');
+				}
+			});
+
+			$('input[name="dokumen"]').change(e => {
+				$('#txt-dokumen').val(e.target.files[0].name);
+			});
+
+			$('#btn-select-file').click(e => {
+				e.preventDefault();
+				$('input[name="dokumen"]').trigger('click');
+			});
+
+			$('#btn-send').click(e => {
+				e.preventDefault();
+				let nama = $('input[name="nama"]');
+				let handphone = $('input[name="handphone"]');
+				let dokumen = $('input[name="dokumen"]');
+				if(nama.val() == '') {
+					nama.closest('.field').addClass('error');
+				}
+				if(handphone.val() == '') {
+					handphone.closest('.field').addClass('error');
+				}
+				if(dokumen.val() == '') {
+					dokumen.closest('.field').addClass('error');
+					dokumen.siblings('.ui.button').addClass('negative');
+				}
+				if(nama.val() == '' || handphone.val() == '' || dokumen.val() == '') return false;
+				$('form').submit();
+			});
+
+		}
+	};
+
 }
 
 _.q = function () {
